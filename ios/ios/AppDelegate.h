e0861c4ed8d7c22567e76b447c71c1cf2ff5caa0
Naohiro OHTA <naohta@gmail.com>
2013-01-18 23:13:51 +0900

    Create iPhone Empty Project using XCode 4.5.2

---
+++ ios/ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ios
+//
+//  Created by Naohiro OHTA on 1/18/13.
+//  Copyright (c) 2013 amaoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
