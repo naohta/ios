b12843b6fbac915e4624c9645150c40c218a4ab0
Naohiro OHTA <naohta@gmail.com>
2013-01-19 00:46:19 +0900

    NSDate things

--- ios/ios/main.m
+++ ios/ios/main.m
@@ -1,18 +1,41 @@
-//
-//  main.m
-//  ios
-//
 //  Created by Naohiro OHTA on 1/18/13.
 //  Copyright (c) 2013 amaoto. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
-
-#import "AppDelegate.h"
 
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        NSLog(@"Hello world!");
+
+        NSDate* now = [[NSDate alloc]init];
+        NSLog(@"%@",now);
+        
+        NSString* s;
+        s = now.description;
+        NSLog(@"%@",s);
+        
+        s = [now descriptionWithLocale:[NSLocale currentLocale]];
+        NSLog(@"%@",s);
+
+        NSTimeInterval i = [now timeIntervalSince1970];
+        NSLog(@"%f",i);
+        
+        NSDateFormatter* f;
+        f = [[NSDateFormatter alloc]init];
+        [f setDateFormat:@"yyyy-MM-dd HH:mm:ss Z"];
+        s = [f stringFromDate:now];
+        NSLog(@"%@",s);
+        
+        f = [[NSDateFormatter alloc]init];
+        [f setDateFormat:@"yyyy-MM-dd HH:mm:ss.SSS Z"];
+        s = [f stringFromDate:now];
+        NSLog(@"%@",s);
+        
+        f = [[NSDateFormatter alloc]init];
+        [f setDateFormat:@"yyyy-MM-dd Z"];
+        NSDate* d = [f dateFromString:@"2013-01-18 +0000"];
+        NSLog(@"%@",d);
+        
     }
 }
